186dedeb0ad58d52dda3d1746603afee39b39e9f
Alexander Swerdlow <aswerdlow1@gmail.com>
2018-07-11 13:27:08 -0700

    Added Controller.cpp

--- model/test.m
+++ model/test.m
@@ -1,9 +1,9 @@
-m = 1140.0;
-lf = 1.165;
-lr = 1.165;
-cf = 155494.663;
-cr = 155494.663;
-Iz = 1436.24;
+m = 4.76;
+lf = 0.35;
+lr = 0.35;
+cf = 150;
+cr = 150;
+Iz = 0.0687;
 vx = 5;
 
 A = [0  1   0   0 
@@ -18,40 +18,10 @@ C = [0 0 0 0;
      0 0 0 0];
 D = [0;
      0];
-
-states = {'x' 'x_dot' 'phi' 'phi_dot'};
-inputs = {'u'};
-outputs = {'x'; 'phi'};
-
-sys_ss = ss(A,B,C,D,'statename',states,'inputname',inputs,'outputname',outputs);
-
-poles = eig(A)
-
-co = ctrb(sys_ss);
-controllability = rank(co)
-
 Q = [1 0 0 0
-    0 1 0 0
-    0 0 1 0
-    0 0 0 1]
-R = 1;
-K = lqr(A,B,Q,R)
-
-Ac = [(A-B*K)];
-Bc = [B];
-Cc = [C];
-Dc = [D];
-
-states = {'x' 'x_dot' 'phi' 'phi_dot'};
-inputs = {'r'};
-outputs = {'x'; 'phi'};
-
-sys_cl = ss(Ac,Bc,Cc,Dc,'statename',states,'inputname',inputs,'outputname',outputs);
-
-t = 0:0.01:5;
-r =0.2*ones(size(t));
-[y,t,x]=lsim(sys_cl,r,t);
-[AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
-set(get(AX(1),'Ylabel'),'String','cart position (m)')
-set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)')
-title('Step Response with LQR Control')
+     0 0 0 0
+     0 0 0 0
+     0 0 0 0];
+ R = 1;
+ 
+ K = lqr(A,B,Q,R);
