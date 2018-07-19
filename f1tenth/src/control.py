7d584eea704b9018f91d452a5e8a58265a54e776
Alexander Swerdlow <aswerdlow1@gmail.com>
2018-07-18 17:39:52 -0700

    Update Control

--- f1tenth/src/control.py
+++ f1tenth/src/control.py
@@ -5,33 +5,34 @@ import math
 from race.msg import drive_param
 from race.msg import pid_input
 from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Twist
 from quant import Quaternion
 
-kp = 20
-kp2 = 0.1667
-kd = 0.09
-prev_error = 0.0 
-vel_input = 25.0
+wheelbase = 0.25
 
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
-def control(data):
-	global prev_error
-	global vel_input
-	global kp
-	global kp2
-	global kd
-	
-def gyro(data):
-	pose = Quaternion(data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
+
+def recieveCommand(data):
+	steering_angle = 0
+	omega = data.angular.z
+	v = data.linear.x
+	if v == 0:
+		steering_angle = 0
+	elif omega == 0:
+		steering_angle = 0
+	else:
+		radius = v/omega
+		steering_angle = math.atan(wheelbase/radius)
+		if data.linear.x < 0:
+			steering_angle *= -1
+	print(math.degrees(steering_angle))
 	msg = drive_param()
-	#msg.velocity = vel_input - (data.pid_error * kp2)
-	msg.velocity = 0
-	msg.angle = 15
+	msg.velocity = data.linear.x
+	msg.angle = steering_angle
 	pub.publish(msg)
 
 if __name__ == '__main__':
 	rospy.init_node('pid_controller', anonymous=True)
-	rospy.Subscriber("error", pid_input, control)
-	rospy.Subscriber("imu", Imu, gyro)
+	rospy.Subscriber("cmd_vel", Twist, recieveCommand)
 	rospy.spin()
