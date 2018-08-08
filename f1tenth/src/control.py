53d631c6afdc5dddde8877057b55e3ef2d9fcfad
Alexander Swerdlow <aswerdlow1@gmail.com>
2018-08-08 10:30:08 -0700

    Update

--- f1tenth/src/control.py
+++ f1tenth/src/control.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
-import math
+import math, time
 from race.msg import drive_param
 import rospy, math
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
 
-wheelbase = 0;
-
+wheelbase = 0.3
+kp = 0.1
+ki = 0.0
+kd = 1.0
+compVel = 0.0
+prevErr = 0.0
+prevtime = 0.0
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
 
@@ -19,20 +26,57 @@ def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
     return math.atan(wheelbase / radius)
 
 
+def quaternion_to_euler_angle(w, x, y, z):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    X = math.degrees(math.atan2(t0, t1))
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    Y = math.degrees(math.asin(t2))
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    Z = math.degrees(math.atan2(t3, t4))
+
+    return X, Y, Z
+
+
 def cmd_callback(data):
     global pub
-
+    global prevtime
+    global prevErr
     v = data.linear.x
     steering = convert_trans_rot_vel_to_steering_angle(v, data.angular.z, wheelbase)
 
+
+
+
+
+def updateOdom(data):
+    try:
+        thet = math.atan(data.twist.twist.linear.y / data.twist.twist.linear.x)
+    except ZeroDivisionError:
+        thet = 0
+    q = euler_from_quaternion(
+        [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w])
+    diff = thet - q[2]
+    vel = math.sqrt(math.pow(data.twist.twist.linear.x, 2) + math.pow(data.twist.twist.linear.y, 2))
+    compVel = vel / math.cos(diff)
+    currentTime = time.clock()
+    err = v - compVel
+    output = (err * kp) + ((err - prevErr) / (currentTime - prevtime))
+    prevtime = currentTime
+    prevErr = err
     msg = drive_param()
-    msg.velocity = v
+    msg.velocity = output
     msg.angle = steering
     pub.publish(msg)
-
 
 if __name__ == '__main__':
     rospy.init_node('Control', anonymous=True)
     rospy.get_param('wheelbase', 0.32385)
     rospy.Subscriber("cmd_vel", Twist, cmd_callback)
+    rospy.Subscriber("odometry/filtered", Odometry, updateOdom)
     rospy.spin()
--- f1tenth/src/controller.cpp
+++ f1tenth/src/controller.cpp
@@ -1,8 +1,79 @@
 #include "ros/ros.h"
-#include "ros/time.h"
-using std
-namespace
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+#include <tf/tf.h>
+#include <race/drive_param.h>
+#include <math.h>
 
-int main(double[][] path) {
+ros::Time prevTime;
+double desiredThrottle, desiredSteering, prevError, wheelbase;
+double kP = 0.1;
+double kI = 0;
+double kD = 1.0;
 
+double getYawFromPose(const geometry_msgs::Pose &carPose) {
+  double x = carPose.orientation.x;
+  double y = carPose.orientation.y;
+  double z = carPose.orientation.z;
+  double w = carPose.orientation.w;
+
+  double tmp, yaw;
+  tf::Quaternion q(x, y, z, w);
+  tf::Matrix3x3 quaternion(q);
+  quaternion.getRPY(tmp, tmp, yaw);
+
+  return yaw;
+}
+
+double convert_trans_rot_vel_to_steering_angle(double v, double omega, double wheelbase) {
+  if (omega == 0 || v == 0) {
+	return 0;
+  }
+  double radius = v / omega;
+  return atan(wheelbase / radius);
+}
+void updateOdom(nav_msgs::Odometry data) {
+  double thet = atan(data.twist.twist.linear.y / data.twist.twist.linear.x);
+
+  double q = getYawFromPose(data.pose.pose);
+  double diff = thet - q;
+  double vel = sqrt(pow(data.twist.twist.linear.x, 2) + pow(data.twist.twist.linear.y, 2));
+  double compVel = vel / cos(diff);
+  ros::Time currentTime = ros::Time::now();
+  double dt = currentTime.toSec() - prevTime.toSec();
+  double err = desiredThrottle - compVel;
+  double output = (err * kP) + ((err - prevError) / (dt));
+  prevTime = currentTime;
+  prevError = err;
+  /*race::drive_param msg;
+  msg.velocity = output;
+  msg.angle = steering;
+  pub.publish(msg); */
+}
+
+void updateCmd(geometry_msgs::Twist data) {
+  desiredThrottle = data.linear.x;
+  desiredSteering = convert_trans_rot_vel_to_steering_angle(data.linear.x, data.angular.z, wheelbase);
+}
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "controller");
+
+  ros::NodeHandle n;
+
+  prevTime = ros::Time::now();
+  desiredThrottle = desiredSteering = prevError = 0;
+  n.param("wheelabase", wheelbase, 0.3);
+
+  ros::Publisher chatter_pub = n.advertise<race::drive_param>("drive_parameters", 1000);
+
+  ros::Rate loop_rate(100);
+
+  ros::Subscriber cmdSub = n.subscribe("cmd_vel", 100, updateCmd);
+  ros::Subscriber odomSub = n.subscribe("cmd_vel", 100, updateOdom);
+
+  ros::spin();
+
+  return 0;
 }
